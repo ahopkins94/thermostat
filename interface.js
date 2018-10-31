649ab1744fba24cc7d362caa872496fa638cefb8
Anouska Hopkins <anouskahopkins@gmail.com>
2018-10-31 15:51:29 +0000

    Added up and down jquery

--- interface.js
+++ interface.js
@@ -1,4 +1,12 @@
 $(document).ready(function() {
   var thermostat = new Thermostat();
   $('#temperature').text(thermostat.temperature);
+  $('#temperature-up').click(function() {
+      thermostat.up();
+      $('#temperature').text(thermostat.temperature);
+  })
+  $('#temperature-down').click(function () {
+    thermostat.down();
+    $('#temperature').text(thermostat.temperature);
+})
 })
