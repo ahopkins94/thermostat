72bdc81944b888a83dd0cabd53c695aa6399e72f
NaomiBid <naomi0bid@gmail.com>
2018-10-31 17:01:27 +0000

    added API key for temperature

--- interface.js
+++ interface.js
@@ -34,4 +34,8 @@ $(document).ready(function() {
     $('#temperature').attr('class', thermostat.energyUsage());
   }
 
+  $.get('http://api.openweathermap.org/data/2.5/weather?q=London&appid=a3d9eb01d4de82b9b8d0849ef604dbed&units=metric', function(data) {
+    $('#temperature').text(data.main.temp);
+  })
+
 })
